43c933531b4be30723441ef77b2cb577dc4a88f8
Diego D. Canuto <diego_domingo1@hotmail.com>
2022-02-02 13:30:29 -0300

    Estudo de TypeScript

---
+++ hello-typescript/main.js
@@ -0,0 +1,12 @@
+var minhaVar = 'minha variavel';
+function minhaFunci(x, y) {
+    return x + y;
+}
+// ES 6 ou ES 2015
+var nume = 1;
+var pii = 2;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
---
+++ hello-typescript/main.ts
@@ -0,0 +1,17 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunciz(x,y) {
+    return x+y;
+}
+
+
+// ES 6 ou ES 2015
+let numez = 1;
+const pizi =2;
+
+var numeros = [1,2,3];
+numeros.map(function(valor) {
+    return valor *2;
+});
+
+numeros.map( valor => valor * 2);
